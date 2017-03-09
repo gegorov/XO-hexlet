fab3e9d3913ba6db3abfcabbd3e7c805fe4911e1
gegorov <gegorov@yahoo.com>
2017-03-09 17:44:59 +0300

    Field - test for Point point added

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,5 +1,8 @@
 package io.hexlet.xo.model;
 
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
+
 import java.awt.*;
 
 /**
@@ -21,11 +24,21 @@ public class Field {
 
     }
 
-    public Figure getFigure (final Point point) {
+    public Figure getFigure (final Point point) throws InvalidPointException{
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
         return field[point.x][point.y];
     }
 
-    public void setFigure (final Point point, final Figure figure) {
+    public void setFigure (final Point point, final Figure figure) throws InvalidPointException,
+                                                                            AlreadyOccupiedException {
+        if (!checkPoint(point)){
+            throw new InvalidPointException();
+        }
+        if (field[point.x][point.y] != null){
+            throw new AlreadyOccupiedException();
+        }
         field[point.x][point.y] = figure;
 
     }
