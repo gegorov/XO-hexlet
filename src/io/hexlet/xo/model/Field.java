19d554c3ee9c9c40109dd6c2204ed7f0b6dcc9a3
gegorov <gegorov@yahoo.com>
2017-03-09 21:13:41 +0300

    Field - methods checkPoint and checkCoordinate were updated for fields of non static size now size of field is created via constructor

--- src/io/hexlet/xo/model/Field.java
+++ src/io/hexlet/xo/model/Field.java
@@ -1,6 +1,5 @@
 package io.hexlet.xo.model;
 
-import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
@@ -10,17 +9,24 @@ import java.awt.*;
  */
 public class Field {
 
-    private final static int FIELD_SIZE = 3;
+
 
     private final  static int MIN_COORDINATE = 0;
 
-    private final  static int MAX_COORDINATE = FIELD_SIZE;
 
-    private final Figure [][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    private final Figure [][] field;
+
+    private final int fieldSize;
+
+    public Field(int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize() {
 
-        return FIELD_SIZE;
+        return fieldSize;
 
     }
 
@@ -41,12 +47,12 @@ public class Field {
     }
 
     private boolean checkPoint( final Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
 
     }
 
-    private boolean checkCoordinate (final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean checkCoordinate (final int coordinate, final int maxCoordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < maxCoordinate;
     }
 
 }
