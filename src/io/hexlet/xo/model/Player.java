670774e1943c2af6408f2a0d3a642986b5c577b9
gegorov <gegorov@yahoo.com>
2017-03-09 16:31:30 +0300

    Player created

---
+++ src/io/hexlet/xo/model/Player.java
@@ -0,0 +1,24 @@
+package io.hexlet.xo.model;
+
+/**
+ * Created by gennadi on 09.03.17.
+ */
+public class Player {
+
+    private final String name;
+
+    private final Figure figure;
+
+    public Player(String name, Figure figure) {
+        this.name = name;
+        this.figure = figure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+}
